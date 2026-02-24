613dcb205f7e9ac597660d74929cd943890be8d0
Juanes <juanklos_86@hotmail.com>
2026-02-24 05:32:49 -0500

    form and functionality added

--- js/index.js
+++ js/index.js
@@ -1,13 +1,29 @@
+
+
 //Create list of skills
 const skills = ["JavaScript", "HTML", "CSS", "Adobe Photoshop", "GitHub"];
 const skillsSection = document.querySelector("#skills");
 const skillsList  = skillsSection.querySelector("ul");
-console.log(console.log(skillsList))
+
+//Special features
+skillsList.style.backgroundColor = "salmon";
+skillsList.style.display = "flex";
+skillsList.style.flexDirection = "column";
+skillsList.style.alignItems = "flex-start";
+skillsList.style.listStyleType = "disc";
+skillsList.style.paddingLeft = "20px";
+console.log(skillsList);
+
+const space = document.createElement('br');
 for (let i = 0; i < skills.length; i++) {
   //console.log(skills[i]);
+  //const space = document.createElement('br');
   const skill = document.createElement('li');
   skill.innerHTML = skills[i];
+  skill.style.display = "list-item";
+  skill.style.float = "none";
   skillsList.appendChild(skill);
+  //skillsList.appendChild(space);
 }
 
 skillsSection.appendChild(skillsList);
@@ -29,9 +45,9 @@ footerElement.style.marginTop = '20px';
 //Goals
 const today = new Date();
 const thisYear = today.getFullYear();
-console.log(thisYear);
+//console.log(thisYear);
 const copyright = document.createElement('p');
-console.log(copyright);
+//console.log(copyright);
 copyright.textContent = `Juan is in ©  ${thisYear}`;
 
 footerElement.appendChild(copyright);
@@ -40,3 +56,57 @@ footerElement.appendChild(copyright);
 document.body.appendChild(footerElement);
 
 
+
+
+
+//******************************* */
+//Asynchronous Programming and Promises
+const messageForm = document.getElementsByName("leave_message");
+messageForm[0].addEventListener('submit', function(event) {
+
+    event.preventDefault(); 
+    const name = event.target.usersName.value;
+    const email = event.target.usersEmail.value;
+    const message = event.target.usersMessage.value;
+    console.log(name);
+    console.log(email);
+    console.log(message);
+
+    const messageSection = document.getElementById("messages");
+    const messageList  = messageSection.querySelector("ul");
+    messageList.style.background = "salmon";
+    messageList.style.backgroundColor = "salmon";
+    messageList.style.display = "flex";
+    messageList.style.flexDirection = "column";
+    messageList.style.alignItems = "flex-start";
+    messageList.style.listStyleType = "disc";
+    messageList.style.paddingLeft = "20px";
+    const newMessage  = document.createElement("li");
+    newMessage.innerHTML = `<a href="mailto:${email}">${name}</a>
+                            <span> ${message} </span>
+                            <br>
+    `;
+    //newMessage.innerHTML = `Algun mensaje`;
+    
+    //Remove button
+    const removeButton  = document.createElement("button");
+    removeButton.innerText = 'remove';
+    removeButton.setAttribute("type", "button");
+    removeButton.addEventListener("click", function(){
+
+      const entry = removeButton.parentNode;
+      console.log("Entry" + entry);
+      entry.remove();
+
+    });
+
+    newMessage.appendChild(space);
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(space);
+    messageList.appendChild(newMessage);
+    if (messageForm) {
+        event.target.reset();
+    }
+
+
+});
